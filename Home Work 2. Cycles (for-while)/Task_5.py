96ea337fae9c530d2637f9e71cee205423a61eda
Anton Obrevko <A_Obrevko@mail.ru>
2023-02-07 00:30:37 +0300

    [SOLVED] Task 5

---
+++ Home Work 2. Cycles (for-while)/Task_5.py
@@ -0,0 +1,12 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+
+def Alg_shuffle(list_of:list):
+    for num in range(0,len(list_of)):
+        list_of.insert(int(random.random()*len(list_of))%len(list_of),list_of.pop())
+
+lis=[1,2,3,4,5,4,3,2,1,4,5,7,6]
+print(lis)
+Alg_shuffle(lis)
+print(lis)
