3cca8768834b2b46666cc4d5e13db51b17e41254
Anton Obrevko <a_obrevko@mail.ru>
2023-02-28 13:55:57 +0300

    [SOLVED] Task 32

--- Home Work 6. List repetition/Task_32.py
+++ Home Work 6. List repetition/Task_32.py
@@ -1,3 +1,13 @@
 # Определить индексы элементов массива (списка), 
 # значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
+import random as R
+import os
+os.system('clear')
+
+mass_list = [R.randint(0,10) for _ in range(11)]
+print("Исходный массив: {}".format(mass_list))
+min,max = input('Введите диапазон через пробел: ').split(' ')
+indexes = [index for index in range(len(mass_list)) if mass_list[index]>=int(min) and mass_list[index]<=int(max)]
+
+print("Индексы элементов массива, значения которых принадлежат диапазону [{},{}]: \n{}".format(min,max,indexes))
